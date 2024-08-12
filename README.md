89a24fe4d0a632007ad14affed756d964b6065fd
ataka <masayuki.ataka@gmail.com>
2024-08-12 18:35:08 +0900

    Move setup instructions into README.md
    from logseq-markdown.el

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # logseq
-Logseq mode for Emacs
+Logseq Markdown mode for Emacs
+
+# Setup
+
+Put the following code into `.dir-locals.el` in your top directory of Logseq.
+
+``` emacs-lisp
+((nil . ((eval
+          (lambda nil
+            (when
+                (and (not (null buffer-file-name))
+                     (string-match-p "\\.md\\'" buffer-file-name)
+                     (not (string= major-mode "logseq-markdown-mode")))
+              (logseq-markdown-mode)))))))
+```
--- logseq-markdown.el
+++ logseq-markdown.el
@@ -1,15 +1,6 @@
 ;;; -*- lexical-binding: t -*-
 ;; Author: Masayuki Ataka <masayuki.ataka@gmail.com>
 ;;
-;; Put the following code into .dir-locals.el in your top directory of Logseq.
-;;
-;; ((nil (eval
-;;        (lambda ()
-;;          (when (and (not (null buffer-file-name))
-;;                     (string-match-p "\\.md\\'" buffer-file-name)
-;;                     (not (string= major-mode "logseq-markdown-mode")))
-;;            (logseq-markdown-mode))))))
-;;
 
 (require 'seq)
 
